d968398179df12b57096b579c268a521963f82a5
unknown <julianmarnewick21@gmail.com>
2022-04-14 21:07:32 +0200

    Join message

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const Discord = require("discord.js");
+const generateImage = require("./generateImage");
 require("dotenv").config()
 
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 
@@ -18,6 +20,16 @@ client.on("messageCreate", (message) => {
     if (message.content == "hello"){
         message.reply("Sup faggot")
     }
+})
+
+const welcomeChannelId = "964238218130251856"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
